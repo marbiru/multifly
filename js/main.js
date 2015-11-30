c254f4d0b35bc68b524bc0050e68f75ff77b4f33
marbiru <code@uribram.com>
2015-11-30 15:08:08 +0700

    win % working
    dummy version of win % for individual myPicks is working

--- js/main.js
+++ js/main.js
@@ -3,4 +3,23 @@ for (var i = 2; i <= 27; i++) {
     myPickArray.push(i);
 };
 
-console.log(myPickArray);
+var myWins = 0;
+var randomWins = 0;
+
+
+var myPick = 6;
+
+for (var randomPick = 2; randomPick <= 27; randomPick++) {
+
+	if (randomPick % myPick == 0) {
+		randomWins += 1;
+	} else if (myPick % randomPick == 0) {
+		myWins += 1;
+	} else if (myPick < randomPick) {
+		myWins += 1;
+	} else if (randomPick < myPick) {
+		randomWins += 1;
+	};
+};
+
+console.log(myWins);
